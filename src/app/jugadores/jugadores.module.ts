ba01c956a8defb104cd8059f7e174e58ec55ad62
Damian Cervantes <dcervantes@ute.com.uy>
2020-08-21 13:03:45 -0300

    btn flotante

--- src/app/jugadores/jugadores.module.ts
+++ src/app/jugadores/jugadores.module.ts
@@ -5,13 +5,19 @@ import { FormularioComponent } from './formulario/formulario.component';
 import { JugadoresService } from '../core/servicios/jugadores.service';
 import { JugadoresRoutingModule } from './jugadores.routing';
 import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { EcoFabSpeedDialModule } from '@ecodev/fab-speed-dial';
 
 @NgModule({
   declarations: [ListaComponent, FormularioComponent],
   imports: [
     CommonModule,
     JugadoresRoutingModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    EcoFabSpeedDialModule
   ],
   providers: [
     JugadoresService
